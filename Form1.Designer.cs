f96dc39b87d7f4df3a23f3acfb256a157d043f97
pjc5767830@gmail.com <pjc5767830@gmail.com>
2026-03-19 12:33:25 +0900

    과제1 코딩과 테스트 완료 리스트박스 텍스트박스 버튼등 각각의 도구들의 이름을 다시 명명해서 깔끔하게 바꾸었고 텍스트박스에 문자를 입력하고 버튼을 누르면 리스트박스에 뜨게끔 코딩하였습니다

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,19 +28,74 @@
         /// </summary>
         private void InitializeComponent()
         {
+            label1 = new Label();
+            lstEchoWindow = new ListBox();
+            txtInput = new TextBox();
+            btnSend = new Button();
             SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("한컴 말랑말랑 Bold", 24F, FontStyle.Bold, GraphicsUnit.Point, 129);
+            label1.ForeColor = Color.Cyan;
+            label1.Location = new Point(0, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(530, 83);
+            label1.TabIndex = 0;
+            label1.Text = "Echo Messenger";
+            // 
+            // lstEchoWindow
+            // 
+            lstEchoWindow.FormattingEnabled = true;
+            lstEchoWindow.Location = new Point(12, 86);
+            lstEchoWindow.Name = "lstEchoWindow";
+            lstEchoWindow.Size = new Size(904, 516);
+            lstEchoWindow.TabIndex = 1;
+            lstEchoWindow.SelectedIndexChanged += listBox1_SelectedIndexChanged;
+            // 
+            // txtInput
+            // 
+            txtInput.Location = new Point(12, 628);
+            txtInput.Multiline = true;
+            txtInput.Name = "txtInput";
+            txtInput.Size = new Size(904, 78);
+            txtInput.TabIndex = 2;
+            txtInput.TextChanged += textBox1_TextChanged;
+            // 
+            // btnSend
+            // 
+            btnSend.BackColor = SystemColors.ActiveCaption;
+            btnSend.Font = new Font("한컴 말랑말랑 Bold", 13.875F, FontStyle.Bold, GraphicsUnit.Point, 129);
+            btnSend.Location = new Point(960, 628);
+            btnSend.Name = "btnSend";
+            btnSend.Size = new Size(190, 78);
+            btnSend.TabIndex = 3;
+            btnSend.Text = "전송";
+            btnSend.UseVisualStyleBackColor = false;
+            btnSend.Click += button1_Click;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(14F, 32F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(2017, 744);
+            ClientSize = new Size(1510, 744);
+            Controls.Add(btnSend);
+            Controls.Add(txtInput);
+            Controls.Add(lstEchoWindow);
+            Controls.Add(label1);
             Name = "Form1";
             Text = "Form1";
             Load += Form1_Load;
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private ListBox lstEchoWindow;
+        private TextBox txtInput;
+        private Button btnSend;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -11,5 +11,22 @@ namespace EchoMessenger
         {
 
         }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string typed_msg = txtInput.Text;
+            lstEchoWindow.Items.Add(typed_msg);
+            txtInput.Clear();
+        }
     }
 }
